35f01b3ac1eac7d41ad340e1bc0325a0d3dbab99
Stefan Niedes <sgniedes@up.edu.ph>
2026-02-10 00:39:49 +0800

    Project initialization

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+// TODO: Add your routes here
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
