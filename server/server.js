306f915735fe6ae9b60b156281f7d524ba459982
Stefan Niedes <sgniedes@up.edu.ph>
2026-02-12 22:49:33 +0800

    Fixed server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const connectDB = require("./config/db"); // 1. Import the connection function (adjust path if it's in a folder)
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// 2. Call the connection
+connectDB(); 
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
-// TODO: Add your routes here
+app.get("/", (req, res) => res.send("API is running..."));
 
 // Start server
 app.listen(PORT, () => {
